acb183a02fff3f8bb32229a18b33fc71227d9940
ewhitebloom <ewhitebloom@gmail.com>
2014-03-01 15:58:51 -0500

    Indent/Mini Fixes

--- server.rb
+++ server.rb
@@ -1,11 +1,10 @@
 require 'sinatra'
-require 'pry'
 require 'shotgun'
 
 set :views, File.dirname(__FILE__) + '/views'
 set :public_folder, File.dirname(__FILE__) + '/public'
 
-      get '/leaderboard' do
+ get '/leaderboard' do
 
         @data = [
   {
@@ -34,31 +33,29 @@ set :public_folder, File.dirname(__FILE__) + '/public'
         }
       ]
 
-        @teams = []
+      @teams = []
 
-        @data.each do |game|
-          game.select { |key,value|
-           if (key == :home_team || key ==:away_team)
-            @flatten  = @teams.map(&:values).flatten
-            unless @flatten.include?(value)
-              @teams << { name: value, wins: 0, losses: 0 }
-            end
+      @data.each do |game|
+        game.select { |key,value|
+         if (key == :home_team || key ==:away_team)
+          @flatten  = @teams.map(&:values).flatten
+          unless @flatten.include?(value)
+            @teams << { name: value, wins: 0, losses: 0 }
           end
+         end
         }
         if  game[:home_score] > game[:away_score]
          win_index = @teams.index{ |team| team[:name] == game[:home_team] }
          lose_index = @teams.index{ |team| team[:name] == game[:away_team] }
          @teams[win_index][:wins] +=  1
          @teams[lose_index][:losses] +=  1
-       end
-       if  game[:home_score] < game[:away_score]
+        elsif  game[:home_score] < game[:away_score]
          lose_index = @teams.index{ |team| team[:name] == game[:home_team] }
          win_index = @teams.index{ |team| team[:name] == game[:away_team] }
          @teams[win_index][:wins] +=  1
          @teams[lose_index][:losses] +=  1
-       end
+        end
      end
-
 
      def win_board(data)
        wins = data.sort_by{ |team| team[:wins] }.reverse
